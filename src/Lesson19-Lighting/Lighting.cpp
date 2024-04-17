bbdba9f08d0d4ef9023306d9f68e39e6e9dd1a7d
文玉 马 <75505898@qq.com>
2024-04-17 19:07:07 +0800

    初始化场景

--- src/Lesson19-Lighting/Lighting.cpp
+++ src/Lesson19-Lighting/Lighting.cpp
@@ -1,42 +1,67 @@
 #include "../Core/Application.h"
 #include "../Entity/Ground.h"
-#include "../Core/Camera.h"
+#include "../Entity/Triangle.h"
+#include "iostream"
+
 #define APPLICTION "Lesson19-Lighting"
 
 class LighingApp :public Application
 {
-
 	// 1.准备数据
 	virtual void Startup() 
 	{		
 		m_ground.Init();
-		//m_camera.SetEye();
-		
+		m_triangle.Init();	
+		m_textureCity   = CreateTexture("textures/chongqing.jpg");
+		m_textureGround = CreateTexture("textures/ground.jpg");
+
+
 	}
 
 	// 2.渲染数据
 	virtual void Render()
 	{
-		glm::mat4 mat1(1);
+		m_ground.Render(m_camera, m_textureGround);	
 
-		m_ground.Render(mat1);
-
+		m_triangle.Render(m_camera);
+		DrawtTiangle();
 	}
-	// 3.关闭
-	virtual void Shutdown()
+	
+
+	// 绘制三角形
+	void DrawtTiangle()
 	{
+	
+
+		//std::cout << glm::to_string(mvp) << std::endl;		
 		
 	}
 
-private: // 变量
+	// 3.关闭
+	virtual void Shutdown()
+	{
+		// 删除纹理
+		glDeleteTextures(1,&m_textureCity);		
+		glDeleteTextures(1,&m_textureGround);
+	}
 
-	Ground m_ground; // 地面
-	Camera m_camera;
+private: // 变量	
+	Ground    m_ground;   // 地面
+	Triangle  m_triangle; // 三角形
+
+
+	unsigned        m_vao = 0;;
+	unsigned        m_vbo = 0;;
+	Shader_P3_C4_T2 m_shader;
+
+	unsigned  m_textureCity   = 0;
+	unsigned  m_textureGround = 0;	
+	
 };
 
 int main()
 {
-	LighingApp lighingApp;
+	LighingApp lighingApp;	
 	lighingApp.Initialize(800,600, APPLICTION);
 	lighingApp.Run();
 }
