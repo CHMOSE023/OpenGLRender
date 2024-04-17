176b92345ad79e32329b29728cc88319e11232bd
文玉 马 <75505898@qq.com>
2024-04-17 19:06:14 +0800

    地面模型

--- src/Entity/Ground.h
+++ src/Entity/Ground.h
@@ -3,69 +3,88 @@
 #include <glm/glm.hpp>
 #include "../Core/Camera.h"
 #include "../Shader/Shader_P3_C4_T2.h"
+#include "../Core/Camera.h"
+
 class Ground :public Entity
 {
-	typedef struct Vertex
-	{
-		glm::vec3 pos;
-		glm::vec4 col;
-		glm::vec2 uv;
-	} Vertex;
-
 public:
 	Ground() 
 	{
+		
 		m_vao = -1;
 		m_vbo = -1;			
 	};
-	~Ground(){};
+	~Ground()
+	{
+		glDeleteVertexArrays(1, &m_vao);
+		glDeleteBuffers(1, &m_vbo);
+	};
 
 	virtual void Init()  // 数据准备
 	{
-		float   gSize = 100;
-		float   gPos = -5;
-		float   rept = 20;
 
-		// 绘制地面数据	
-		static const Vertex grounds[6] =
-		{
-			//       顶点                    颜色                   UV                
-		   { { -gSize, gPos,-gSize }, { 1.f, 1.f, 1.f,1.0f },{  0.0f,0.0f } },
-		   { {  gSize, gPos,-gSize }, { 1.f, 1.f, 1.f,1.0f },{  rept,0.0f } },
-		   { {  gSize, gPos, gSize }, { 1.f, 1.f, 1.f,1.0f },{  rept,rept }},
+		 typedef struct Vertex
+		 {
+		 	glm::vec3 pos;
+		 	glm::vec4 col;
+		 	glm::vec2 uv;
+		 } Vertex;
 
-		   { { -gSize, gPos,-gSize }, { 1.f, 1.f, 1.f,1.0f },{  0.0f,0.0f } },
-		   { {  gSize, gPos, gSize }, { 1.f, 1.f, 1.f,1.0f },{  rept,rept } },
-		   { { -gSize, gPos, gSize }, { 1.f, 1.f, 1.f,1.0f },{   0.0f,rept } }
 
-		};
+		 float   gSize = 100;
+		 float   gPos = -5;
+		 float   rept = 20;
 
-		m_shader.Initialize(); // 初始化shader		
+		 // 绘制地面数据	
+		 static const Vertex grounds[6] =
+		 {
+			 //       顶点                    颜色                   UV                
+			{ { -gSize, gPos,-gSize }, { 1.f, 1.f, 1.f,1.0f },{  0.0f,0.0f } },
+			{ {  gSize, gPos,-gSize }, { 1.f, 1.f, 1.f,1.0f },{  rept,0.0f } },
+			{ {  gSize, gPos, gSize }, { 1.f, 1.f, 1.f,1.0f },{  rept,rept }},
+
+			{ { -gSize, gPos,-gSize }, { 1.f, 1.f, 1.f,1.0f },{  0.0f,0.0f } },
+			{ {  gSize, gPos, gSize }, { 1.f, 1.f, 1.f,1.0f },{  rept,rept } },
+			{ { -gSize, gPos, gSize }, { 1.f, 1.f, 1.f,1.0f },{   0.0f,rept } }
+
+		 };
+
+		 // 初始化shader
+		 m_shader.Initialize();
+
+		 // 创建显存并向顶点缓冲填充数据
 	
+		 glGenBuffers(1, &m_vbo);
+		 glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+		 glBufferData(GL_ARRAY_BUFFER, sizeof(grounds), grounds, GL_STATIC_DRAW);
 
-		// 创建显存并向顶点缓冲填充数据	
-		glGenBuffers(1, &m_vbo);
-		glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(grounds), grounds, GL_STATIC_DRAW);
-
-		glGenVertexArrays(1, &m_vao);
-		glBindVertexArray(m_vao);
-		glEnableVertexAttribArray(m_shader.m_position);
-		glVertexAttribPointer(m_shader.m_position, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
-		glEnableVertexAttribArray(m_shader.m_color);
-		glVertexAttribPointer(m_shader.m_color, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)12);
-		glEnableVertexAttribArray(m_shader.m_uv);
-		glVertexAttribPointer(m_shader.m_uv, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)28);
-		glBindVertexArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		 glGenVertexArrays(1, &m_vao);
+		 glBindVertexArray(m_vao);
+		 glEnableVertexAttribArray(m_shader.m_position);
+		 glVertexAttribPointer(m_shader.m_position, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+		 glEnableVertexAttribArray(m_shader.m_color);
+		 glVertexAttribPointer(m_shader.m_color, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)12);
+		 glEnableVertexAttribArray(m_shader.m_uv);
+		 glVertexAttribPointer(m_shader.m_uv, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)28);
+		 glBindVertexArray(0);
+		 glBindBuffer(GL_ARRAY_BUFFER, 0);
+		
 
 	}
 
 
-	virtual void Render(glm::mat4 &mat4) // 绘制
+	virtual void Render(Camera &camera, unsigned texture) // 绘制
 	{
-		glm::mat4 mvp= mat4;
+
+		glm::mat4  mvp;
+		glm::mat4  matView = camera.GetView();
+		glm::mat4  matProj = camera.GetProject();
+		glm::mat4  matModel = glm::mat4x4(1.0);
+
+		mvp = matProj * matView * matModel;
+		
 		m_shader.Begin();
+		glBindTexture(GL_TEXTURE_2D, texture);   // 使用纹理2
 		glBindVertexArray(m_vao);
 		glUniformMatrix4fv(m_shader.m_mvp, 1, GL_FALSE, (const GLfloat*)&mvp);
 		glDrawArrays(GL_TRIANGLES, 0, 6);
