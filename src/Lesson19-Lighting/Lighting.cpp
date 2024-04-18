95ff2e9c65fc408d75f9c97a046c601641941020
CHMOSE023 <75505898@qq.com>
2024-04-18 15:40:03 +0800

    物体基础颜色

--- src/Lesson19-Lighting/Lighting.cpp
+++ src/Lesson19-Lighting/Lighting.cpp
@@ -2,31 +2,49 @@
 #include "../Entity/Ground.h"
 #include "../Entity/Triangle.h"
 #include "iostream"
-#include "../Entity/LightBox.h"
+#include "../Entity/Box.h"
+#include "BoxColor.h"
 
 #define APPLICTION "Lesson19-Lighting"
 
 class LighingApp :public Application
 {
+ 
+
 	// 1.准备数据
 	virtual void Startup() 
-	{		
+	{	
 		m_textureCity   = CreateTexture("textures/chongqing.jpg");
 		m_textureGround = CreateTexture("textures/ground.jpg");
+        
+		m_triangle.Init();		
+		m_box.Init();
+		m_boxColor.Init();     
 
-		m_triangle.Init();
 		m_ground.Init();
 		m_ground.SetTexture(m_textureGround);
-		m_lightBox.Init();
 
 	}
+
+
 
 	// 2.渲染数据
 	virtual void Render()
 	{
-		m_ground.Render(m_camera);	
-		m_triangle.Render(m_camera);
-		m_lightBox.Render(m_camera);
+         // glm::mat4 model(1.0f);
+         // glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+         // glm::mat4 projection = glm::perspective(glm::radians(45.0f), (GLfloat)m_winWidth / (GLfloat)m_winHeight, 0.1f, 100.0f);
+        
+         m_ground.Render(m_camera);
+         m_triangle.Render(m_camera);
+
+		 m_boxColor.Render(m_camera);
+		 m_box.Render(m_camera);
+       
+
+       
+
+
 	}
 	
 	// 3.关闭
@@ -40,10 +58,14 @@ class LighingApp :public Application
 private: // 变量	
 
 	// 模型类
-	Ground    m_ground;   // 地面
-	Triangle  m_triangle; // 三角形
-	LightBox  m_lightBox;
+	Ground       m_ground;   // 地面
+	Triangle     m_triangle; // 三角形
+	Box          m_box;      // 彩色盒子
+	BoxColor     m_boxColor; // 物体原始颜色
 
+	
+
+   
 	unsigned  m_textureCity   = 0;
 	unsigned  m_textureGround = 0;	
 	
@@ -52,6 +74,6 @@ private: // 变量
 int main()
 {
 	LighingApp lighingApp;	
-	lighingApp.Initialize(1920 * 0.5, 1080 * 0.5, APPLICTION);
+	lighingApp.Initialize(1920 * 1, 1080 * 1, APPLICTION);
 	lighingApp.Run();
 }
