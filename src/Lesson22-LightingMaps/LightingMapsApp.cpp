0c22745b67ef4cfec4fa55b5af04f885541b82e1
CHMOSE023 <75505898@qq.com>
2024-04-19 17:08:15 +0800

    纹理贴图使用案例。

--- src/Lesson22-LightingMaps/LightingMapsApp.cpp
+++ src/Lesson22-LightingMaps/LightingMapsApp.cpp
@@ -1,13 +1,18 @@
+
 #include "../Core/Application.h"
 #include "../Entity/Entity.h"
 #include <vector>
 #include "../Entity/Ground.h"
+#include "../Entity/BoxLightingMaps.h"
 
 class LightingMapsApp :public Application
 {
 public:
 	LightingMapsApp() {};
-	~LightingMapsApp() {};
+	~LightingMapsApp() 
+	{
+
+	};
 		
 	// 1.准备数据
 	virtual void Startup()
@@ -15,12 +20,23 @@ public:
 		// 准备纹理数据
 		m_textureCity   = CreateTexture("textures/chongqing.jpg");
 		m_textureGround = CreateTexture("textures/ground.jpg");
+		m_diffuseMap    = CreateTexture("textures/container2.png");
+		m_specularMap   = CreateTexture("textures/container2_specular.png");
 
 		// 地面
 		Ground* pGround = new Ground();
 		pGround->Init();
 		pGround->SetTexture(m_textureGround);
 		entities.push_back(pGround);
+
+		BoxLightingMaps* pBoxLightingMaps =new BoxLightingMaps;
+		// 创建纹理材质
+		pBoxLightingMaps->m_diffuseMap  = m_diffuseMap;
+		pBoxLightingMaps->m_specularMap = m_specularMap;
+
+		pBoxLightingMaps->Init();
+
+		entities.push_back(pBoxLightingMaps);
 	}
 
 	// 2.渲染数据
@@ -52,6 +68,9 @@ private:
 	std::vector <Entity*> entities;  // 模型数组
 	unsigned  m_textureCity = 0;
 	unsigned  m_textureGround = 0;
+	unsigned  m_diffuseMap = 0;
+	unsigned  m_specularMap = 0;
+	
 
 };
 
